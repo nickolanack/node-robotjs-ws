2eaa240cb899e8d4eec812bebdb6ea41f96b41fa
nickolanack <nickblackwell82@gmail.com>
2015-08-11 01:57:11 -0700

    add keyTapArray use up keys (keyTap) at an interval

--- keyboard.js
+++ keyboard.js
@@ -19,13 +19,33 @@ Keyboard.prototype.longKeyPress=function(key, cb, time){
 	
 	setTimeout(function(){
 		
-		clearTimeout(i);
+		clearInterval(i);
 		me.rb.keyToggle(key, false);
 		if((typeof cb)=='function')cb();
 	},time);
+	
+	return me;
 }
 
 
+Keyboard.prototype.keyTapArray=function(keys, cb, delay){
+	var me=this;
+	var i=setInterval(function(){
+		
+		me.rb.keyTap(keys.shift());
+		if(keys.length==0){
+			clearInterval(i);
+			if((typeof cb)=='function')cb();
+		}
+		
+	},delay);
+	
+	return me;
+}
+
+
+
+//made this to test long key press with only two keyToggles
 Keyboard.prototype.longKeyPressSimple=function(key, cb, time){
 	var me=this;
 
